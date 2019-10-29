9bc955b6e2fa2f1774ccc376ffd4b19db9d1ea33
chikavi1 <chikavi@hotmail.com>
2019-10-29 15:21:29 -0600

    modifique el background ubi

--- src/pages/background/background.ts
+++ src/pages/background/background.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { ToastController } from 'ionic-angular';
 import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationEvents, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
 
 @IonicPage()
@@ -18,13 +18,13 @@ export class BackgroundPage {
             stopOnTerminate: false, // Si pones este en verdadero, la aplicación dejará de trackear la localización cuando la app se haya cerrado.
     };
   constructor(public navCtrl: NavController, public navParams: NavParams,
-          private backgroundGeolocation: BackgroundGeolocation) {
+          private backgroundGeolocation: BackgroundGeolocation,private toastCtrl: ToastController) {
   	this.locations  = [];
 
   	this.backgroundGeolocation.configure(this.config)
           .then(() => {
             this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
-              this.locations.push(location);
+              this.presentToast(location);
               console.log(location);
               // Es muy importante llamar al método finish() en algún momento para que se le notifique al sistema que hemos terminado y que libere lo que tenga que liberar,
               // Si no se hace, la aplicación dará error por problemas de memoria.
@@ -41,4 +41,13 @@ export class BackgroundPage {
   	this.backgroundGeolocation.stop();
   }
  
+ presentToast(location) {
+  let toast = this.toastCtrl.create({
+    message: "lat: "+location.latitude+" lng: "+location.longitude,
+    duration: 1000,
+    position: 'top'
+  });
+
+  toast.present();
+}
 }
